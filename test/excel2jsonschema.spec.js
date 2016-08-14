9e62085f57133aba38410cf299976aaa896f2c95
pponugo <pponugo@gmail.com>
2016-08-14 16:22:44 -0400

    mocha and travis

---
+++ test/excel2jsonschema.spec.js
@@ -0,0 +1,9 @@
+var assert = require('assert')
+
+describe('The excel2jsonschema', function() {
+  describe('when init project', function() {
+    it('should be true', function() {
+      assert.equal('true', 'true')
+    })
+  })
+})
